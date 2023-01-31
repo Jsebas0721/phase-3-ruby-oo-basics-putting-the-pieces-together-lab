c57b1bc91b79525fac787984a2e63ca06b388cd1
sebastian medina <sebastianlopez_21@hotmail.com>
2023-01-30 22:23:23 -0500

    commit tree 2bd3f2a16a33d19f8b49ec7524251ac0d169c7fc

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
 
+    def initialize(title)
+        @title = title     
+    end
 
+     def turn_page
+        puts "Flipping the page...wow, you read fast!"
+     end
+
+end
+
+b1 = Book.new("Book 1")
+b1.author = "Agatha Christie"
